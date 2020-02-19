4babbba5dc738022535541ee904a31c853e3fa2e
MinKyeongTae <kyeongtea92@gmail.com>
2020-02-20 02:31:43 +0900

    Add Methods for Test Editing MemoData, Arrange Codes in App

--- LineMemoWithNoStoryboard/LineMemoUITests/LineMemoUITests.swift
+++ LineMemoWithNoStoryboard/LineMemoUITests/LineMemoUITests.swift
@@ -9,7 +9,9 @@
 import XCTest
 
 class LineMemoUITests: XCTestCase {
-    let app = XCUIApplication() // 테스트를 위한 UIApplication
+    // MARK: - Property
+
+    private let app = XCUIApplication() // 테스트를 위한 UIApplication
 
     private let sampleImageURL = "https://homepages.cae.wisc.edu/~ece533/images/cat.png"
 
@@ -33,41 +35,46 @@ class LineMemoUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    // MARK: - 메모 데이터 확인 + 삭제 테스트
+    // MARK: - TEST Method
 
-    /// * 존재 메모데이터 순차 확인 및 제거 로직 테스트
-    func testDeletingMemoData() {
-        let memoTableView = app.tables.matching(identifier: XCTIdentifier.MainMemoView.memoTableView).firstMatch
+    // MARK: - 메모편집 로직 테스트
 
-        // MARK: 셀 메모 데이터 순차 확인 & 삭제
+    func testEditingMemoDataWithoutImage() {
+        let memoDataCells = app.tables.matching(identifier: XCTIdentifier.MainMemoView.memoTableView).cells
+        XCTAssert(memoDataCells.count > 0, "There's no memoDataCell")
 
-        while memoTableView.cells.count > 0 {
-            sleep(1)
-            checkFirstTableViewCellData(element: memoTableView)
-            deleteFirstMemoTableViewCell(element: memoTableView)
-        }
+        let firstCell = memoDataCells.element(boundBy: 0)
+        firstCell.tap()
+
+        app.buttons["편집"].tap()
+        editMemoText()
+        hideKeyboard()
+        app.buttons["저장"].tap()
+        app.buttons["메모 리스트"].tap()
+
+        sleep(1)
     }
 
-    // MARK: - 이미지 x + 메모 추가 테스트
+    // MARK: - 이미지 x + 메모추가 로직 테스트
 
     /// * 이미지 없이 메모 추가 로직 테스트
     func testAddingMemoDataWithoutImage() {
-        // MARK: AddMemoView 이동
+        // AddMemoView 이동
 
         pressAddMemoBarButton()
 
-        // MARK: 메모내용 입력
+        // 메모내용 입력
 
         insertMemoText(title: "Insert Title Text", subText: "Insert Sub Text")
         hideKeyboard()
         sleep(1)
 
-        // MARK: 메모저장
+        // 메모저장
 
         saveMemo()
         sleep(1)
 
-        // MARK: 저장메모 확인
+        // 저장메모 확인
 
         selectMainMemoTableViewLastRowCell()
     }
@@ -76,28 +83,28 @@ class LineMemoUITests: XCTestCase {
 
     /// * URL 이미지 등록 포함 메모 추가 로직 테스트
     func testAddingMemoDataWithURLImage() {
-        // MARK: AddMemoView 이동
+        // AddMemoView 이동
 
         pressAddMemoBarButton()
 
-        // MARK: 메모 URL Image 추가
+        // 메모 URL Image 추가
 
         pressAddImageCell()
         presentAddImageURLView()
         getURLImage(url: sampleImageURL)
 
-        // MARK: 메모내용 입력
+        // 메모내용 입력
 
         insertMemoText(title: "Insert Title Text", subText: "Insert Sub Text")
         hideKeyboard()
         sleep(1)
 
-        // MARK: 메모저장
+        // 메모저장
 
         saveMemo()
         sleep(1)
 
-        // MARK: 저장메모 확인
+        // 저장메모 확인
 
         selectMainMemoTableViewLastRowCell()
     }
@@ -106,31 +113,60 @@ class LineMemoUITests: XCTestCase {
 
     /// * 앨범 이미지 등록 포함 메모 추가 로직 테스트
     func testAddingMemoDataWithAlbumImage() {
-        // MARK: AddMemoView 이동
+        // AddMemoView 이동
 
         pressAddMemoBarButton()
 
-        // MARK: 메모 Image 선택
+        // 메모 Image 선택
 
         pressAddImageCell()
         mornitorAlbumAuthAlertController()
         getAlbumImage()
 
-        // MARK: 메모내용 입력
+        // 메모내용 입력
 
         insertMemoText(title: "Insert Title Text", subText: "Insert Sub Text")
         hideKeyboard()
         sleep(1)
 
-        // MARK: 메모저장
+        // 메모저장
 
         saveMemo()
         sleep(1)
 
-        // MARK: 저장메모 확인
+        // 저장메모 확인
 
         selectMainMemoTableViewLastRowCell()
     }
+
+    // MARK: - 메모 데이터 확인 + 삭제 테스트
+
+    /// * 존재 메모데이터 순차 확인 및 제거 로직 테스트
+    func testDeletingMemoData() {
+        let memoTableView = app.tables.matching(identifier: XCTIdentifier.MainMemoView.memoTableView).firstMatch
+        XCTAssert(memoTableView.cells.count > 0, "There's no cell to delete")
+
+        // 셀 메모 데이터 순차 확인 & 삭제
+
+        while memoTableView.cells.count > 0 {
+            sleep(1)
+            checkFirstTableViewCellData(element: memoTableView)
+            deleteFirstMemoTableViewCell(element: memoTableView)
+        }
+    }
+
+    // MARK: - 앱 실행 테스트
+
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+
+    // MARK: - Event Method
 
     private func checkFirstTableViewCellData(element: XCUIElement) {
         let firstCell = element.cells.element(boundBy: 0)
@@ -153,7 +189,7 @@ class LineMemoUITests: XCTestCase {
         urlTextField.typeText(imageURL)
         addURLImageButton.tap()
 
-        if !editMemoView.waitForExistence(timeout: TimeInterval(10.0)) {
+        if !editMemoView.waitForExistence(timeout: TimeInterval(30.0)) {
             fatalError("There is Long Delay while getting URL Image")
         }
     }
@@ -204,12 +240,33 @@ class LineMemoUITests: XCTestCase {
         subTextView.typeText(subText)
     }
 
+    private func editMemoText(title: String = "editedText", subText: String = "editedText") {
+        let titleTextField = app.textFields.matching(identifier: XCTIdentifier.EditMemoView.titleTextField).firstMatch
+        let subTextView = app.textViews.matching(identifier: XCTIdentifier.EditMemoView.subTextView).firstMatch
+
+        titleTextField.tap()
+        selectAllText(element: titleTextField)
+        titleTextField.typeText(title)
+        subTextView.tap()
+        selectAllText(element: subTextView)
+        subTextView.typeText(subText)
+    }
+
+    private func selectAllText(element: XCUIElement) {
+        element.press(forDuration: TimeInterval(2.0))
+        app.menuItems["Select All"].tap()
+    }
+
     private func selectCollectionViewItemCell(at index: Int) {
+        let collectionViewCells = app.collectionViews.firstMatch.cells
+        XCTAssert(collectionViewCells.count > 0, "There's no Item Cell")
         app.collectionViews.firstMatch.cells.element(boundBy: index).tap()
     }
 
     private func selectTableViewCell(at index: Int) {
-        app.tables.cells.element(boundBy: index).tap()
+        let tableViewCells = app.tables.cells
+        XCTAssert(tableViewCells.count > 0, "There's no Cell")
+        tableViewCells.element(boundBy: index).tap()
     }
 
     private func pressButton(identifier: String) {
@@ -226,16 +283,7 @@ class LineMemoUITests: XCTestCase {
 
     private func selectMainMemoTableViewLastRowCell() {
         let mainMemoTableView = app.tables.matching(identifier: XCTIdentifier.MainMemoView.memoTableView)
-        if mainMemoTableView.cells.count == 0 { return }
+        XCTAssert(mainMemoTableView.cells.count > 0, "There's no memoDataCell")
         mainMemoTableView.cells.element(boundBy: mainMemoTableView.cells.count - 1).tap()
-    }
-
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
